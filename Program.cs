168edb5d481ad05c02b16ac8f04159420f7aab55
Lucas Feitoza <jlkzfp@gmail.com>
2022-12-24 14:04:49 -0300

    Finalizado desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,10 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia = new Nokia("12345", "A1", "123123123123", 16);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+
+Iphone iphone = new Iphone("1114444", "13S", "2222333", 8);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
